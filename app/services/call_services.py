8ebfb517a2c5e1d37a5a277973cf79848632718b
keerthan sai parupally <keerthansai214@gmail.com>
2026-03-09 20:34:24 +0530

    mistake

--- app/services/call_services.py
+++ app/services/call_services.py
@@ -30,7 +30,7 @@ def initiate_outbound_call(phone_number, call_id):
         from_=from_number,
         url=voice_url,
         status_callback_event=status_url,
-        status_callbck_events=["initiated", "ringing", "answered", "completed"],
+        status_callback_events=["initiated", "ringing", "answered", "completed"],
         status_callback_method="POST"
     )
 
