713a406ffdd0dd09994cb7601264dc9149b92c33
keerthan sai parupally <keerthansai214@gmail.com>
2026-03-11 15:21:41 +0530

    addes CORS options

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
-    CORS(app)
+    CORS(app,resources={r"/*":{"origins":"*"}})
 
     # Register routes later
     from .routes import register_routes
