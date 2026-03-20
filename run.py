7d2a0bdc47f7bb708a97f4919907fe53c15a93e3
keerthan sai parupally <keerthansai214@gmail.com>
2026-03-21 00:04:05 +0530

    fix Procfile port binding

--- run.py
+++ run.py
@@ -1,36 +1,7 @@
-# from app import create_app, db
-
-# app = create_app()
-
-# with app.app_context():
-#     db.create_all()
-
-# if __name__ == "__main__":
-# #     app.run(port=8090, debug=True)
-
-# import logging
-# from app import create_app, db
-
-# # Configure logging
-# logging.basicConfig(
-#     level=logging.INFO,
-#     format="%(asctime)s - %(levelname)s - %(message)s"
-# )
-
-# app = create_app()
-
-# with app.app_context():
-#     db.create_all()
-
-# if __name__ == "__main__":
-#     logging.info("Starting Flask server...")
-#     # app.run(port=8090, debug=True)
-#     app.run(host='0.0.0.0', port=8090, debug=True)
-
+# 
 import logging
 from app import create_app, db
 
-# Configure logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
@@ -48,3 +19,5 @@ with app.app_context():
 if __name__ == "__main__":
     logging.info("Starting Flask server...")
     app.run(host='0.0.0.0', port=8090, debug=True)
+
+
