3e090f72d1aa4aa94d1e9e40328023a4bfe5be6d
keerthan sai parupally <keerthansai214@gmail.com>
2026-03-21 00:57:05 +0530

    done chnages in env

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,14 +3,24 @@ import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider,signInWithPopup,signOut,onAuthStateChanged} from 'firebase/auth';
 
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAbPzHU1H7B6d0brgALHXegrxOCqWA6GlU",
+//   authDomain: "outreachaicalling.firebaseapp.com",
+//   projectId: "outreachaicalling",
+//   storageBucket: "outreachaicalling.firebasestorage.app",
+//   messagingSenderId: "589301280148",
+//   appId: "1:589301280148:web:8ffd1e760f99b42654f381",
+//   measurementId: "G-8R047BFS6Q"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAbPzHU1H7B6d0brgALHXegrxOCqWA6GlU",
-  authDomain: "outreachaicalling.firebaseapp.com",
-  projectId: "outreachaicalling",
-  storageBucket: "outreachaicalling.firebasestorage.app",
-  messagingSenderId: "589301280148",
-  appId: "1:589301280148:web:8ffd1e760f99b42654f381",
-  measurementId: "G-8R047BFS6Q"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,  
 };
 
 // Initialize Firebase
